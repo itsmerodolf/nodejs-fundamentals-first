e49ae0b68e1e46900e95eac8ef81c0b0c9b78111
itsmerodolf <DotDot_12000@yahoo.com>
2018-02-05 15:12:47 +0800

    added initial files

---
+++ challenge1node/app.js
@@ -0,0 +1,5 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
---
+++ challenge1node/notes.js
@@ -0,0 +1,6 @@
+console.log('Starting notes.js');
+
+module.exports.addNote = function () {
+  console.log('addNote');
+  return 'New note';
+};
