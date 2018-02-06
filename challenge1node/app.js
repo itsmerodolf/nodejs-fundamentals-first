19d6e4dc1f53d7d6a683ee1432ede3c4b5a2885d
itsmerodolf <DotDot_12000@yahoo.com>
2018-02-06 12:48:49 +0800

    simplified inputs with Yargs

--- challenge1node/app.js
+++ challenge1node/app.js
@@ -3,6 +3,7 @@ console.log('Starting app.js');
 const fs = require('fs');
 const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
 const notes = require('./notes.js');
 
 /*applying add function in app.js
@@ -16,20 +17,26 @@ var filteredArray = _.uniq(['Rodolf', 1, 'Rodolf', 2, 3, 4]);
 console.log(filteredArray);
 */
 
-/* Getting input from user
+// Getting input from user
+ const argv = yargs.argv;
  var command = process.argv[2];
  console.log('Command:', command);
+ //console.log('Process', process.argv);
+ console.log('Yargs', argv);
  //console.log(process.argv);
 
  if (command === 'add') {
-   console.log('Adding new Note');
+   //console.log('Adding new Note');
+   notes.addNote(argv.title, argv.body);
  } else if (command === 'list') {
-   console.log('Listing all notes');
+   //console.log('Listing all notes');
+   notes.getAll();
  } else if (command === 'read') {
-   console.log('Reading note');
+   //console.log('Reading note');
+   notes.getNote(argv.title);
  } else if (command === 'remove') {
-   console.log('Removing note');
+   //console.log('Removing note');
+   notes.removeNote(argv.title);
  }else {
    console.log('Command not recognized');
  }
-*/
--- challenge1node/notes.js
+++ challenge1node/notes.js
@@ -1,12 +1,35 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = function () {
+/*module.exports.addNote = function () {
   console.log('addNote');
   return 'New note';
-};
+}; */
 
 /* defining utility function in notes.js
 module.exports.add = (a, b) => {
   return a + b;
 };
 */
+
+var addNote = (title, body) => {
+    console.log('Adding note', title, body);
+};
+
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+  console.log('Getting Note', title);
+}
+
+var removeNote = (title) => {
+  console.log('Removing Note', title);
+}
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+};
