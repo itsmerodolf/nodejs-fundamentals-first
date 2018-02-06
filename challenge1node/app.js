c4bee890faca61d9183dd2f2fbf1297db64adcb6
itsmerodolf <DotDot_12000@yahoo.com>
2018-02-06 11:30:54 +0800

    Getting input from user

--- challenge1node/app.js
+++ challenge1node/app.js
@@ -15,3 +15,21 @@ console.log(_.isString('Andrew'));
 var filteredArray = _.uniq(['Rodolf', 1, 'Rodolf', 2, 3, 4]);
 console.log(filteredArray);
 */
+
+/* Getting input from user
+ var command = process.argv[2];
+ console.log('Command:', command);
+ //console.log(process.argv);
+
+ if (command === 'add') {
+   console.log('Adding new Note');
+ } else if (command === 'list') {
+   console.log('Listing all notes');
+ } else if (command === 'read') {
+   console.log('Reading note');
+ } else if (command === 'remove') {
+   console.log('Removing note');
+ }else {
+   console.log('Command not recognized');
+ }
+*/
